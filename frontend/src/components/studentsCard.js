08a6b7a5ce58ec2729c757861003e11b5f8c30c8
expog2000 <hariom@pearlthoughts.com>
2024-04-11 13:23:10 +0530

    fix ui issue

--- frontend/src/components/studentsCard.js
+++ frontend/src/components/studentsCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const StudentCard = ({ name, DOB, feesPaid, onStudentDetailClick }) => {
-    console.log("name",name)
+    
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-blue-200">
       <div className="px-6 py-4">
